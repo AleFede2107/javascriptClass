c3666f5b9aa92328bdee4b8942d94f423a11cb5b
Federico <alegriafederico0@gmail.com>
2024-08-12 19:47:21 -0600

    (:sparkles:) add string.js

---
+++ strings.js
@@ -0,0 +1,19 @@
+let firstName ='Federico';
+let nationality = 'Salvadoreno';
+
+//console.log(firstName);
+//console.log(nationality);
+
+//console.log('Hello '+firstName+' Good Morning');
+
+//console.log(`Hello ${firstName} Good Morning`);
+
+let phrase = 'Camaron que se duerme se lo lleva la corriente';
+
+//console.log(phrase.length);
+
+//console.log(phrase.toLowerCase);
+
+//console.log(phrase.toUpperCase);
+
+//console.log(phrase.toLocaleLowerCase('en-US'));
