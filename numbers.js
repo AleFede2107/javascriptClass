9413dc618a7cd00697238738b52079958e4a2b42
Federico <alegriafederico0@gmail.com>
2024-08-12 20:03:45 -0600

    feat (:sparkles:) add numbers.js file

---
+++ numbers.js
@@ -0,0 +1,36 @@
+let manzanas = 4;
+let libraPapa = 1.60;
+
+//console.log(manzanas);
+//console.log(libraPapa);
+
+//console.log(typeof manzanas);
+
+//notacion cientifica
+let atomoAgua = 2.5e8;
+
+//infinito
+let miAmorPorElla = infinity;
+
+//Not a Number
+let noEsNumero = NaN;
+
+//Operadores aritmeticos
+//suma
+let suma = 20 + 5;
+console.log(`El resultado de la suma es ${suma}`);
+
+//resta
+let resta = 20-9;
+
+//multiplicacion
+let multiplicacion = 2*3;
+
+//division
+let division = 10/2;
+
+//raiz cuadrada
+let raiz = Math.sqrt(16);
+
+//random
+let random = Math.random();
