37b71a58d5b3dfffafcce4b880e8f591172f7267
Federico <alegriafederico0@gmail.com>
2024-08-12 19:23:28 -0600

    (:sparkles:) add variables javascript file

---
+++ variables.js
@@ -0,0 +1,14 @@
+let pais = "El Salvador";
+const pi = 3.1416;
+//deprecado
+var departamento = "San Salvador";
+
+// COSAS NO PERMITIDAS
+
+//let x = 'El Salvador';
+// let 1pais = 'El salvador'
+// let ps = 'El Salvador'
+
+// CAMEL-CASE
+let nombreCompleto = 'Fernando';
+
