03ec2f4414db0f7dd6cb036676c41c9067bdbb60
Federico <alegriafederico0@gmail.com>
2024-08-12 19:31:15 -0600

    (:sparkles:) add data-types.js

---
+++ data-types.js
@@ -0,0 +1,23 @@
+//String
+let nombre = 'Carlos Portillo';
+
+//Number
+let edad = 19;
+
+//BOOLEAN
+let estaVivo = true;
+let asistencia = false;
+let activo = true;
+let mayorEdad = true;
+
+//NULL
+let nombreCompleto = null;
+
+//UNDEFINED
+let genero = undefined;
+
+//symbol
+let id = Symbol('mi-unico-id');
+
+//bigint
+let numero = 100n;
