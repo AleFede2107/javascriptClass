5a1809faaa21a3372078815273cc9c6cdbc5baec
Federico <alegriafederico0@gmail.com>
2024-08-13 19:20:13 -0600

    :sparkles: (index.html) add mini game

--- hello.js
+++ hello.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+//Si el numero secreto es igual al numero que ingresaste ganaste
+//si el numero secreto es mayor al que ingresaste
+//si el numero secreto es menor al que ingresaste
+
+let secretNumber = Math.floor(Math.random()*10 + 1)
+
+let guessedNumber = prompt('Adivina el numero secreto entre (0 y 10)');
+console.log(guessedNumber);
+
+if(secretNumber == guessedNumber){
+    console.log('You Won')
+}else if(secretNumber < guessedNumber){
+    console.log('Your guess is higher')
+}else {
+    console.log('Your guess is lower')
+};
