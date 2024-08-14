1bf5cffded2e690756b0ef8d8f1a50a888634238
Federico <alegriafederico0@gmail.com>
2024-08-13 19:05:25 -0600

    :sparkles: (conditionals.js) add conditionals lesson

---
+++ conditionals.js
@@ -0,0 +1,35 @@
+/* let age = 25;
+
+if(age >= 18){
+    console.log("Es mayor de edad")
+}else{
+    console.log("Es menor de edad")
+;} */
+
+//Variable de notas, si la nota es igual o mayor a 9 excelente
+//si la nota es mayor o igual a 6 aprobado
+//si la nota es menor a 6 reprobado
+
+/* let grade = 10;
+
+if(grade >= 9){
+    console.log("Excelente")
+} else if( grade >= 6){
+    console.log(Aprobado)
+} else{
+    console.log(Reprobado)
+} */
+
+//definir una variable que contenga una letra de la a-d
+// si la variable tiene el valor 'b' o 'B'imprimir en pantalla has pulsado la letra B
+
+/* let letter = "c"
+
+if(letter == 'b' || letter == 'B'){
+    console.log('Has pulsado la letra "B"')
+}else {
+    console.log("Has presionado otra letra")
+} */
+
+//TERNARIO
+/* age >= 18 ? console.log('Eres mayor de edad') : console.log('Eres menor de edad'); */
