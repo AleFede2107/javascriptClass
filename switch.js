1999f7d69227a7a5686b62ee47961b309c99f8c3
Federico <alegriafederico0@gmail.com>
2024-08-13 19:44:15 -0600

    :sparkles: feat (switch.js) add little prompt game

--- switch.js
+++ switch.js
@@ -1,4 +1,4 @@
-let paymentMethod = 'cash'
+/* let paymentMethod = 'cash'
 
 switch(paymentMethod){
     case 'cash':
@@ -15,4 +15,26 @@ switch(paymentMethod){
 
     default:
         console.log('Invalid payment method')
+} */
+
+// Desde el teclado van a pedir una serie de cursos:
+// HTML CSS Javascript
+// imprimir el curso seleccionado y validar la entrada de datos
+
+let entryValue = prompt('Seleccione un curso:');
+
+let evaluationValue = entryValue.toUpperCase;
+
+switch(entryValue){
+    case 'HTML':
+        console.log('seleccionaste HTML')
+        break
+    case 'CSS':
+        console.log('seleccionaste CSS')
+        break
+    case 'JAVASCRIPT':
+        console.log('seleccionaste JAVASCRIPT')
+        break
+    default :
+        console.log('Este no es uno de los cursos disponibles')
 }
