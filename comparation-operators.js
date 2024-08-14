750ac21410bb2dd30a5d6fe242850485f9b7f3b9
Federico <alegriafederico0@gmail.com>
2024-08-13 18:24:01 -0600

    :sparkles: (comparation-operators) add javascript file to study the comparation operators

---
+++ comparation-operators.js
@@ -0,0 +1,18 @@
+let firstValue = 10;
+let secondValue = 5;
+
+/* console.log(firstValue == secondValue);
+console.log(firstValue === secondValue); */
+
+//console.log(firstValue != secondValue);
+
+//console.log(firstValue > secondValue);
+
+//console.log(firstValue < secondValue);
+
+//console.log(firstValue <= secondValue);
+
+//console.log(firstValue >= secondValue);
+
+//console.log(firstValue == secondValue);
+//console.log(firstValue === secondValue);
