a2c12340b692bd4b8d0a02c8d226baa1ad264c8b
Federico <alegriafederico0@gmail.com>
2024-08-13 19:59:35 -0600

    :sparkles: feat (loops.js) add expication for js loops

---
+++ loops.js
@@ -0,0 +1,23 @@
+// Ciclo for
+
+/* for(let i = 0; i <= 10; i++){
+    console.log('El numero es: '+ i)
+}; */
+
+//While
+
+/* let i = 0;
+
+while(i<= 10){
+    console.log('El numero es: ' + i)
+    i++
+} */
+
+// Do While
+
+/* let j = 0;
+
+do{
+    console.log(`El numero es: ${j}`)
+    j++
+}while(j <= 10) */
